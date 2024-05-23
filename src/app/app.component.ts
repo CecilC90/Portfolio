d427cb1ba9aeaf5d6df7b06183f6be08a1e0d71c
CecilC90 <c.cigan@gmx.net>
2024-05-23 21:09:35 +0200

    create atf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { AtfComponent } from '../mainContent/atf/atf.component';
+
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, AtfComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
