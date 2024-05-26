e229ced442853404889167a920ea82a0be81be5d
CecilC90 <c.cigan@gmx.net>
2024-05-26 11:49:57 +0200

    add mySkill component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { AtfComponent } from '../mainContent/atf/atf.component';
+import { AboutMeComponent } from '../mainContent/about-me/about-me.component';
+import { MySkillsComponent } from '../mainContent/my-skills/my-skills.component';
 
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent, AtfComponent],
+  imports: [HeaderComponent, AtfComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
